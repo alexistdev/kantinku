4db4429a1f622af940fc98cbf8dc8427fad6d323
Alexsander Hendra Wijaya <alexistdev@gmail.com>
2023-02-04 19:30:28 +0700

    Android

---
+++ web-admin/database/migrations/2023_02_04_045553_create_detailtransaksis_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('detailtransaksis', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('transaksi_id')
+                ->constrained('transaksis')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('menu_id')
+                ->constrained('menus')
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('jumlah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('detailtransaksis');
+    }
+};
